917829536abebb744ed0acf4374573c3afc199f2
Dmytro Shnurenko <dmytro.shnurenko@resmed.com>
2021-08-28 10:41:13 +0300

    Add file bytes streaming example

--- webflux/src/test/java/org/asgard/WebFluxTest.java
+++ webflux/src/test/java/org/asgard/WebFluxTest.java
@@ -1,9 +1,22 @@
 package org.asgard;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.scheduler.Schedulers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.time.Duration;
+import java.util.function.Consumer;
 
 public class WebFluxTest {
 
@@ -22,10 +35,50 @@ public class WebFluxTest {
                    .filter(s -> s.length() > 3)
                    .timeout(Duration.ofMillis(1000))
                    .subscribe(System.out::println, ex -> System.out.println(ex.getMessage()), () -> System.out.println("completed"));
+
+    System.out.println("main thread exit!");
   }
 
   @Test
   public void performingFluxLogicInDifferentThread_NonBlockingMainThread() {
+    Flux<String> messageProducer = Flux.just("This", "is", "java", "world", "dude");
+    messageProducer.publishOn(Schedulers.single())
+                   .timeout(Duration.ofSeconds(1000))
+                   .subscribe(System.out::println, ex -> System.out.println(ex.getMessage()), () -> System.out.println("completed"));
 
+    System.out.println("main thread exit!");
+  }
+
+  @Test
+  public void createStreamOfBytesFromTheFile() {
+    Flux<Byte> messageProducer = Flux.create(fileBytesStreamConsumer());
+    messageProducer
+//       publishOn performs streaming processing in different thread
+        .publishOn(Schedulers.single())
+        .subscribe(System.out::println, ex -> System.out.println(ex.getMessage()), () -> System.out.println("completed"));
+
+    System.out.println("End main thread!");
+  }
+
+  private Consumer<FluxSink<Byte>> fileBytesStreamConsumer() {
+    return sink -> {
+      ByteBuffer buffer = ByteBuffer.allocate(24);
+
+      try (RandomAccessFile file = new RandomAccessFile("src/test/resources/test.txt", "r");
+           FileChannel channel = file.getChannel()) {
+
+        while (buffer.hasRemaining()) {
+          channel.read(buffer);
+          buffer.flip();
+
+          for (byte b : buffer.array()) {
+            sink.next(b);
+          }
+        }
+        sink.complete();
+      } catch (IOException e) {
+        sink.error(e);
+      }
+    };
   }
 }
